820f623cc8cbc89fb514b20b320fcb8b42078f3f
sbdm05 <s.baibou@gmail.com>
2021-12-19 21:17:59 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'newsApp';
+  category: any; 
+  listeArticles: [] = [];
+
+  constructor( private http : HttpClient){}
+
+  onSubmit(){
+    console.log(this.category)
+
+    // appel
+    // api key e743931a18e81262cac54dfe5af6daab
+    this.http
+        .get<any>(`http://api.mediastack.com/v1/news?access_key=e743931a18e81262cac54dfe5af6daab&countries=fr&categories=${this.category}`)
+        .subscribe(res =>{
+          this.listeArticles = res.data;
+          console.log(this.listeArticles)
+        })
+
+  }
+
 }
